68543b7c42318eea3233f37b15cc968a97470668
Miroslav Nožička <st67079@upce.cz>
2021-10-04 15:45:08 +0200

    Add cv.3

---
+++ Main_1.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Cv1 exercises
+        System.out.print("Gallons: ");
+        double gallons = input.nextDouble();
+        System.out.println("Litres: " + gallons * 3.7854);
+
+        System.out.print("Kilos on Earth: ");
+        double kilos = input.nextDouble();
+        System.out.println("Kilos on the Moon: " + kilos * 0.17);
+
+        System.out.print("Circle radius: ");
+        double radius = input.nextDouble();
+        System.out.println("Circle circumference: " + 2 * Math.PI * radius);
+        System.out.println("Circle area: " + Math.PI * Math.pow(radius, 2));
+
+        // Cv2 exercises
+        System.out.print("Jméno a Příjmení: ");
+        input.nextLine(); // consume new line
+        String name = input.nextLine();
+        System.out.println("Jméno: " + name);
+
+        System.out.print("Zadejte číslo: ");
+        int num = input.nextInt();
+        if (num > 0) {
+            System.out.println("Cislo je kladne.");
+        } else if (num == 0) {
+            System.out.println("Cislo je nula.");
+        } else {
+            System.out.println("Cislo je zaporne.");
+        }
+
+        System.out.print("cislo 1: ");
+        int numF = input.nextInt();
+        System.out.print("cislo 2: ");
+        int numS = input.nextInt();
+        System.out.print("cislo 3: ");
+        int numT = input.nextInt();
+
+        if (numF > numS && numF > numT) {
+            System.out.println("Max: " + numF);
+        } else if (numS > numF && numS > numT) {
+            System.out.println("Max: " + numS);
+        } else {
+            System.out.println("Max" + numT);
+        }
+    }
+}
---
+++ Main_3.java
@@ -0,0 +1,89 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        // 1. Načtěte celočíselnou hodnotu -> pomocí konstrukce 'switch'
+        // vyberte odpovídající měsíc (např.: 1 -> Leden, 2 - Únor, etc..) a vypište (pokud je hodnota mimo rozsah, vypište "Neplatná hodnota")
+        System.out.print("Zadejte měsíc: ");
+        int input = sc.nextInt();
+
+        switch (input) {
+            case 1:
+                System.out.println("Leden");
+                break;
+            case 2:
+                System.out.println("Únor");
+                break;
+            case 3:
+                System.out.println("Březen");
+                break;
+            case 4:
+                System.out.println("Duben");
+                break;
+            case 5:
+                System.out.println("Květen");
+                break;
+            case 6:
+                System.out.println("Červen");
+                break;
+            case 7:
+                System.out.println("Červenec");
+                break;
+            case 8:
+                System.out.println("Srpen");
+                break;
+            case 9:
+                System.out.println("Září");
+                break;
+            case 10:
+                System.out.println("Říjen");
+                break;
+            case 11:
+                System.out.println("Listopad");
+                break;
+            case 12:
+                System.out.println("Prosinec");
+                break;
+            default:
+                System.out.println("Neplatná hodnota");
+        }
+
+        // 2. Vypočet faktoriálu vámi zadaného čísla. Výsledek vypiště. - Řeště pomocí 'for' cyklu
+        System.out.print("Zadejte číslo:");
+        int fac = sc.nextInt();
+        int result = 1;
+        for (int i = fac; i != 0; i--) {
+            result *= i;
+        }
+        System.out.println("Faktoriál je: " + result);
+
+        // 3. Proveďte celočíselné dělení vámi zadného dělence a dělitele. - Řeště pomocí 'while' cyklu
+        System.out.print("Zadejte delenec: ");
+        int delenec = sc.nextInt();
+        System.out.print("Zadejte delitel: ");
+        int delitel = sc.nextInt();
+        result = 0;
+        while (delenec >= delitel) {
+            result++;
+            delenec -= delitel;
+        }
+        System.out.println("Výsledek dělení: " + result);
+
+        // 4. Najdětě největšího společného dělitele dvou čísel - pomocí 'do while' cyklu
+        System.out.print("Zadejte první číslo: ");
+        int firstN = sc.nextInt();
+        System.out.print("Zadejte druhé číslo: ");
+        int secondN = sc.nextInt();
+        int div = firstN < secondN ? secondN : firstN;
+        div += 1;
+        do {
+            div--;
+        } while (firstN % div != 0 || secondN % div != 0);
+        System.out.println("Největší společný dělitel je: " + div);
+    }
+}
